a34b2db946ceca8429254782779ff81046a86be6
Loqy <Vincent.Ullmann@googlemail.com>
2014-06-28 01:23:49 +0200

    Fixed import vuScene...

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-from SceneSelector import utils
+from vuSceneSelector import utils
 
 
 ##############################################################################################
